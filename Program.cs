adb98ab545f353ede20a1cbca6b8c8e02e4fb443
Shaun Houck <houcks08@gmail.com>
2022-04-04 10:25:35 -0600

    Initial commit. Added HTMLAgilityPack and created simple scraper to grab mounts from https://ffxiv.consolegameswiki.com website

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FFXIVScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scraper scraper = new Scraper();
+            scraper.GetItem();
+        }
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FFXIVScraper
+{
+    class Scraper
+    {
+        public void GetItem()
+        {
+
+            string itemName = GetName();
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load($"https://ffxiv.consolegameswiki.com/wiki/{itemName}");
+            var HeaderNames = doc.DocumentNode.SelectNodes("//div[@class='mw-parser-output']/p");
+            foreach (var item in HeaderNames)
+            {
+                if (item.InnerText != "\n")
+                {
+                    Console.WriteLine(item.InnerText);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private string GetName()
+        {
+            Console.Write("Enter item name: ");
+            string itemName = Console.ReadLine();
+            itemName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(itemName);
+            itemName = itemName.Replace(" ", "_");
+            return itemName;
+        }
+
+    }
+}
